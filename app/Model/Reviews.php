651f2b6e11f3f9e060c42b991522a32ced9d0d6b
donprinceso <donprince4life44@gmail.com>
2019-07-08 00:33:02 +0100

    modifed the Product and reviews

--- app/Model/Reviews.php
+++ app/Model/Reviews.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Model;
-
+use App\Model\Product;
 use Illuminate\Database\Eloquent\Model;
 
 class Reviews extends Model
 {
-    //
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
