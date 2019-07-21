8a4d10588d00998bd82b7813a94a5b8b421975f1
donprinceso <donprince4life44@gmail.com>
2019-07-21 10:21:05 +0100

    get The review create

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,86 +2,83 @@
 
 namespace App\Http\Controllers;
 
-use App\Model\Review;
-use App\Model\Product;
-use Illuminate\Http\Request;
+use App\Http\Requests\ReviewRequest;
 use App\Http\Resources\ReviewResource;
+use App\Model\Product;
+use App\Model\Review;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class ReviewController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     * * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function index($id,Product $product)
-    {
-       return ReviewResource::collection($product->find($id)->reviews);
-    }
+class ReviewController extends Controller {
+	/**
+	 * Display a listing of the resource.
+	 * * @param  int  $id
+	 * @return \Illuminate\Http\Response
+	 */
+	public function index($id, Product $product) {
+		return ReviewResource::collection($product->find($id)->reviews);
+	}
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+	/**
+	 * Show the form for creating a new resource.
+	 * * @return \Illuminate\Http\Response
+	 */
+	public function create() {
+		//
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @return \Illuminate\Http\Response
+	 */
+	public function store(ReviewRequest $request, $id, product $product) {
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Model\Review  $reviews
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Review $reviews)
-    {
-        //
-    }
+		$review = new Review($request->all());
+		$product->find($id)->reviews()->save($review);
+		return response([
+				'data' => new ReviewResource($review)
+			], Response::HTTP_CREATED);
+	}
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  \App\Model\Review  $reviews
+	 * @return \Illuminate\Http\Response
+	 */
+	public function show(Review $reviews) {
+		//
+	}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Reviews  $reviews
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Review $reviews)
-    {
-        //
-    }
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  \App\Model\Reviews  $reviews
+	 * @return \Illuminate\Http\Response
+	 */
+	public function edit(Review $reviews) {
+		//
+	}
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Review  $reviews
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Review $reviews)
-    {
-        //
-    }
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \App\Model\Review  $reviews
+	 * @return \Illuminate\Http\Response
+	 */
+	public function update(Request $request, Review $reviews) {
+		//
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Model\Review  $reviews
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Review $reviews)
-    {
-        //
-    }
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  \App\Model\Review  $reviews
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy(Review $reviews) {
+		//
+	}
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -4,10 +4,9 @@ namespace App\Model;
 use App\Model\Product;
 use Illuminate\Database\Eloquent\Model;
 
-class Review extends Model
-{
-    public function product()
-    {
-        return $this->belongsTo('App\Model\Product');
-    }
+class Review extends Model {
+	public function product() {
+		return $this->belongsTo('App\Model\Product');
+	}
+	protected $fillable = ["customer", "star", "review"];
 }
